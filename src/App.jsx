a8ecb3cd8487c98f662a6ab957f6f801b7b4c147
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-03-18 15:02:46 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 import {React,useState} from "react";
 import RegisterPage from "./views/pages/RegisterPage";
+import EmailVerification from "./views/pages/EmailVerification";
 import "./index.css"
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RegisterPage />} />
+      <Route path="/" element={<Navigate replace to="/auth/register-page" />} />
+        <Route path="/auth/register-page" element={<RegisterPage />} />
+        <Route path="/emailverification" element={<EmailVerification/>}/>
       </Routes>
     </Router>
   );
