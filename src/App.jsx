e232a99533826c6bd6d6280715d9cb18d5816e1d
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-03-18 17:38:55 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-do
 import {React,useState} from "react";
 import RegisterPage from "./views/pages/RegisterPage";
 import EmailVerification from "./views/pages/EmailVerification";
+import { Toaster } from "react-hot-toast";
 import "./index.css"
 
 function App() {
   return (
+    
     <Router>
+      <div><Toaster position="top-right" reverseOrder={false} /></div>
       <Routes>
       <Route path="/" element={<Navigate replace to="/auth/register-page" />} />
         <Route path="/auth/register-page" element={<RegisterPage />} />
